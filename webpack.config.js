a666c37867653f1c3818b1e9af6197fab5020833
Huy Tran <hoang.huy.tran@gmail.com>
2018-02-18 12:50:51 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+};
